acf998494d4eba7b32c33ec8221acfaba563292a
ShariqShaikh33 <shariqshaikh2705@gmail.com>
2024-12-02 22:10:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let btn = document.getElementById("submit");
+
+btn.addEventListener('click',function(){
+	let fontsize = document.getElementById("fontsize").value;
+	let fontcolor = document.getElementById("fontcolor").value;
+
+	document.cookie = `font-size=${fontsize}`;
+	
+});
